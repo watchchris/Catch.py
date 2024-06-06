ca1feb6a2bef7d1ecde8e43aabea05f24ede4975
Chris Johnson <chris.johnson2@bsu.edu>
2024-06-05 20:45:35 -0500

    Add files via upload

--- Chris_Johnson_Catch.py
+++ Chris_Johnson_Catch.py
@@ -5,19 +5,51 @@ Catch - Slide
 
 import random, pygame, simpleGE
 
+class Money(simpleGE, Sprite):
+    def __init__(self, scene)
+        super().__init__(scene)
+        set.setImage("Money.png")
+        self.setSize(25, 25)
+        self.minSpeed = 3
+        self.maxSpeed = 8
+
+
+    def reset(self):
+        self.y = 10
+
+        #x is random to screen width
+        self.x = random.randint(0, self.screenWidth)
+
+        #dy is random minSpeed to maxSpeed
+        self.dy = randomrandint(self.minSpeed, self.maxSpeed)
+
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
+
 class Chris(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
         self.setImage("Chris.png")
-        self.setsize(50, 50)
+        self.setSize(50, 50)
         self.position = (320, 400)
+        self.moveSpeed = 5
+
+    def process(self):
+        if self.isKeyPresses(pygame.K_LEFT):
+            self.x -= self.moveSpeed
+        if self.isKeyPresses(pygame.K_RIGHT):
+            self.x += self.moveSpeed
 
 class Game(simpleGE, Scene):
     def __init__(self):
         super().__init__()
         self.setImage("skyline.jpg")
         self.chris = Chris(self)
-        self.sprites = (self.chris)
+        self.Money = Money(self)
+
+        self.sprites = (self.chris,
+                        self.money)
 
 
 def main():
